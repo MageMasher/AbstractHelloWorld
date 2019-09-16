3461570c010d80ff0017a990599faeede8e6091e
Joe Lane <me@joelane.net>
2019-09-16 09:56:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+
+        ITool tool = new Tool();
+
+
+        System.out.println("tool.getRootFile() = " + tool.getRootFile());
+    }
+}
---
+++ src/Spec.java
@@ -0,0 +1,8 @@
+abstract class Spec {
+
+    private String rootFile = "Im the string that represents rootFile";
+
+    public String getRootFile() {
+        return this.rootFile;
+    }
+}
